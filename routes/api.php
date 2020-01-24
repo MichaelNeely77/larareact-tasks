d4c796e66e877f286cbe05e3dff9e95300c1eac2
Michael Neely <michael@michaelneely.net>
2020-01-23 23:34:26 -0500

    Still working with integrating the API

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::get('tasks', 'TaskController@index');
-Route::get('tasks', 'TaskController@show');
+Route::get('tasks/{id}', 'TaskController@show');
 Route::post('tasks', 'TaskController@store');
-Route::put('tasks', 'TaskController@update');
-Route::delete('tasks', 'TaskController@delete');
+Route::put('tasks/{id}', 'TaskController@update');
+Route::delete('tasks/{id}', 'TaskController@delete');
